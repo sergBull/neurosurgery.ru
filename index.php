e9374c0fa7045ba46094672bafeba70e22d28e16
sergBull <bullsergv@gmail.com>
2018-02-01 18:16:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	echo "<h2>Сегодня вышла демо-запись альбома &quot;Красная Волна&quot;</h2><br/>";
 	echo "<p>Сегодня вышла демо-запись альбома &quot;Красная Волна&quot; молодой рязанской группы Нейрохирургия, в которую входят Артемий Денисенко и Сергей Булеков. Список композиций приведён ниже";
 	echo "<br/><audio controls src='audios/Нейрохирургия - Винтовка (cover to Гражданская Оборона).mp3'/><br/>";
-	echo "<img src='imgs/Cover.png'/><br/>";
+	echo "<img src='images/Cover.png'/><br/>";
 	echo "<audio controls src='audios/Нейрохирургия - Пролетарская.mp3'/><br/>";
 	echo "<audio controls src='audios/Нейрохирургия - Вечный скот.mp3'/>";
 	echo "<br/><i>01.02.2018:19:08</i></p>";
